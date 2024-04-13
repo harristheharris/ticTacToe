efc35891a8c9dc2c27bf5234b670d92a3d1373cc
Brandon Carter <brandon.d.carter99@gmail.com>
2024-04-13 19:43:00 -0400

    Make Player names alternatively toggle visibility

--- assets/javascript.js
+++ assets/javascript.js
@@ -10,7 +10,6 @@ const bottomLeft = document.getElementById("item7");
 const bottomMiddle = document.getElementById("item8");
 const bottomRight = document.getElementById("item9");
 
-
 strtBttn.addEventListener("click", startGame);
 topLeft.addEventListener("click", boxSelection);
 topMiddle.addEventListener("click", boxSelection);
@@ -48,14 +47,12 @@ function startGame(event) {
   });
 }
 
+function boxSelection(event) {
+  turnToggle(playerTurn);
+  console.log("clicked!");
+}
 
-function boxSelection(event){
-    turnToggle(playerTurn);
-    console.log('clicked!');
-
-};
-
-//as of now, we have a start button and a board that is displayed once the start button is pressed. 
+//as of now, we have a start button and a board that is displayed once the start button is pressed.
 //what else do we need??
 //We choose to have turns alternate and therefore, the symbol place to alternate
 //with this in mind I want a turn indication to appear for whose turn it is
@@ -64,17 +61,24 @@ function boxSelection(event){
 //when the start button is pressed player1 turn is set right away to play
 //when a square is selcted to place a symbol the turn is switched
 function turnToggle(turn) {
-    let toggleTurns = $(".turns");
-    console.log(toggleTurns);
-    toggleTurns.each(toggle());
+  // let toggleTurns = $(".turns");
+  // console.log(toggleTurns);
 
-    if (turn === 0){
-        
-        return playerTurn = 1;
-    } else {
-        return playerTurn = 0; 
-    }
+  if (turn === 0) {
+    return (playerTurn = 1);
+  } else {
+    return (playerTurn = 0);
+  }
 }
 
-turnToggle(playerTurn)
-
+turnToggle(playerTurn);
+$(".box").click(function () {
+  $("#player-one").css(
+    "visibility",
+    $("#player-one").css("visibility") == "hidden" ? "visible" : "hidden"
+  );
+  $("#player-two").css(
+    "visibility",
+    $("#player-two").css("visibility") == "hidden" ? "visible" : "hidden"
+  );
+});
